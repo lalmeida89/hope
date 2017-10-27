2563f2280f9f5c22dc503fb61903e4eea8084a40
lalmeida89 <lalmeida89@gmail.com>
2017-10-27 14:42:22 -0400

    hahaaaaaaa

---
+++ conf.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'APP_KEY': 'dj0yJmk9cUI1MnAwbnpKNVJmJmQ9WVdrOVpHOWxlRUZOTnpnbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yZA--',
+  'APP_SECRET': '4bb3b39ff676ac703578b89d359c2719a06a59b2',
+  'APP_URL': 'https://powerful-falls-37929.herokuapp.com'
+};
